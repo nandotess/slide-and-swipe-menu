ef57982462eed7601fde0808bd0454fa7cfd0974
Fernando Tessmann <nandotess85@gmail.com>
2017-06-23 09:01:21 -0300

    - Plugin entire recoded with jQuery plugin development boilerplate: github.com/jquery-boilerplate/jquery-boilerplate - Now it's possible have many instances from types, each one with its configurations

--- jquery.slideandswipe.js
+++ jquery.slideandswipe.js
@@ -1,170 +1,191 @@
-/**
- * Slide and swipe menu (https://github.com/JoanClaret/slide-and-swipe-menu)
- *
- * @copyright Copyright 2013-2015 Joan claret
- * @license   MIT
- * @author    Joan Claret Teruel <dpam23 at gmail dot com>
- *
- * Licensed under The MIT License (MIT).
- * Copyright (c) Joan Claret Teruel <dpam23 at gmail dot com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the 'Software'), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-
-;(function($, document, window, undefined) {
-
-    'use strict';
-
-    var slideAndSwipe =
-
-        $.fn.slideAndSwipe = function(options) {
-
-            var nav = $(this); // get the element to swipe
-            var navWidth = -nav.outerWidth();
-            var transInitial = navWidth;
-
-            // get settings
-            var settings = $.extend({
-                triggerOnTouchEnd   : true,
-                swipeStatus         : swipeStatus,
-                allowPageScroll     : 'vertical',
-                threshold           : 100,
-                excludedElements    : 'label, button, input, select, textarea, .noSwipe',
-                speed               : 250
-
-            }, options );
-
-            nav.swipe(settings);
-
-            /**
-             * Catch each phase of the swipe.
-             * move : we drag the navigation
-             * cancel : open navigation
-             * end : close navigation
-             */
-            function swipeStatus(event, phase, direction, distance) {
-                if(phase == 'start') {
-                    if(nav.hasClass('ssm-nav-visible')) {
-                        transInitial = 0;
-                    } else {
-                        transInitial = navWidth;
-                    }
-                }
-                var mDistance;
-
-                if (phase == 'move' && (direction == 'left')) {
-                    if(transInitial < 0) {
-
-                        mDistance = transInitial - distance;
-                    } else {
-                        mDistance = -distance;
-                    }
-
-                    scrollNav(mDistance, 0);
-
-                } else if (phase == 'move' && direction == 'right') {
-                    if(transInitial < 0) {
-                        mDistance = transInitial + distance;
-                    } else {
-                        mDistance = distance;
-                    }
-                    scrollNav(mDistance, 0);
-                } else if (phase == 'cancel' && (direction == 'left') && transInitial === 0) {
-                    scrollNav(0, settings.speed);
-                } else if (phase == 'end' && (direction == 'left')) {
-
-                       hideNavigation();
-                } else if ((phase == 'end' || phase == 'cancel') && (direction == 'right')) {
-                    console.log('end');
-                }
-            }
-
-            /**
-             * Browser detect
-             */
-            function isSafari() {
-                return /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
-            }
-
-            function isChrome() {
-                return /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-            }
-
-            /**
-             * Manually update the position of the nav on drag
-             */
-            function scrollNav(distance, duration) {
-                nav.css('transition-duration', (duration / 1000).toFixed(1) + 's');
-
-                if(distance >= 0) {
-                    distance = 0;
-                }
-                if(distance <= navWidth) {
-                    distance = navWidth;
-                }
-                if(isSafari() || isChrome()) {
-                   nav.css('-webkit-transform', 'translate(' + distance + 'px,0)');
-                }
-                else{
-                   nav.css('transform', 'translate(' + distance + 'px,0)');
-                }
-                if(distance == '0') {
-                    $('.ssm-toggle-nav').addClass('ssm-nav-visible');
-                    $('html').addClass('is-navOpen');
-                    $('.ssm-overlay').fadeIn();
-                }
-            }
-
-            /**
-             * Open / close by click on burger icon
-             */
-            var hideNavigation = (function() {
-                nav.removeClass('ssm-nav-visible');
-                scrollNav(navWidth, settings.speed);
-                $('html').removeClass('is-navOpen');
-                $('.ssm-overlay').fadeOut();
-            });
-
-            var showNavigation = (function() {
-                nav.addClass('ssm-nav-visible');
-                scrollNav(0, settings.speed);
-            });
-
-            $('.ssm-toggle-nav').click(function(e) {
-                if(nav.hasClass('ssm-nav-visible')) {
-                    hideNavigation();
-                }
-                else{
-                    showNavigation();
-                }
-                e.preventDefault();
-            });
-        }
-    ;
-})(window.jQuery || window.$, document, window);
-
-
-
 /*
- * Export as a CommonJS module
+ *  Slide and swipe menu - v1.0.0
+ *  https://github.com/nandotess/slide-and-swipe-menu
+ *
+ *  Made by Fernando Tessmann
+ *  Under MIT License
+ *
+ *  Forked from: https://github.com/JoanClaret/slide-and-swipe-menu
+ *  jQuery plugin development boilerplate: https://github.com/jquery-boilerplate/jquery-boilerplate
  */
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = slideAndSwipe;
-}
+
+;(function($, window, document, undefined) {
+
+	'use strict';
+
+	var pluginName = 'slideAndSwipe',
+		defaults = {
+			triggerOnTouchEnd: true,
+			swipeStatus: function() {},
+			allowPageScroll: 'vertical',
+			threshold: 100,
+			excludedElements: 'label, button, input, select, textarea, .noSwipe',
+			speed: 250
+		};
+
+	function Plugin(element, options) {
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.element = element;
+		this.settings = $.extend({}, defaults, options);
+		this.init();
+	}
+
+	$.extend(Plugin.prototype, {
+
+		/**
+		 * Plugin initialiser
+		 */
+		init: function() {
+			var self;
+
+			this.nav = $(this.element);
+			this.navWidth = - this.nav.outerWidth();
+			this.transInitial = this.navWidth;
+			this.navRel = this.nav.attr('rel');
+
+			if (this.navRel) {
+				this.navRel = '[rel="' + this.navRel + '"]';
+			} else {
+				this.navRel = '';
+			}
+
+			this.navCta = $('.ssm-toggle-nav' + this.navRel);
+
+			this.settings.swipeStatus = this.swipeStatus;
+			this.nav.swipe(this.settings);
+
+			self = this;
+
+			this.navCta.click(function(e) {
+				if (self.nav.hasClass('ssm-nav-visible')) {
+					self.hideNavigation();
+				} else {
+					self.showNavigation();
+				}
+
+				e.preventDefault();
+			});
+		},
+
+		/**
+		 * Destroy the instance
+		 */
+		destroy: function() {
+			this.nav.removeData('plugin_' + pluginName);
+		},
+
+		/**
+		 * Browser detect: Safari
+		 */
+		isSafari: function() {
+			return /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+		},
+
+		/**
+		 * Browser detect: Chrome
+		 */
+		isChrome: function() {
+			return /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+		},
+
+		/**
+		 * Catch each phase of the swipe.
+		 * move : we drag the navigation
+		 * cancel : open navigation
+		 * end : close navigation
+		 */
+		swipeStatus: function(event, phase, direction, distance) {
+			var self = this.data('plugin_' + pluginName);
+
+			if (phase == 'start') {
+				if (self.nav.hasClass('ssm-nav-visible')) {
+					self.transInitial = 0;
+				} else {
+					self.transInitial = self.navWidth;
+				}
+			}
+
+			var mDistance;
+
+			if (phase == 'move' && (direction == 'left')) {
+				if(self.transInitial < 0) {
+					mDistance = self.transInitial - distance;
+				} else {
+					mDistance = - distance;
+				}
+
+				self.scrollNav(mDistance, 0);
+			} else if (phase == 'move' && direction == 'right') {
+				if (self.transInitial < 0) {
+					mDistance = self.transInitial + distance;
+				} else {
+					mDistance = distance;
+				}
+
+				self.scrollNav(mDistance, 0);
+			} else if (phase == 'cancel' && (direction == 'left') && self.transInitial === 0) {
+				self.scrollNav(0, self.settings.speed);
+			} else if (phase == 'end' && (direction == 'left')) {
+				self.hideNavigation();
+			} else if ((phase == 'end' || phase == 'cancel') && (direction == 'right')) {
+				console.log('end');
+			}
+		},
+
+		/**
+		 * Manually update the position of the nav on drag
+		 */
+		scrollNav: function(distance, duration) {
+			this.nav.css('transition-duration', (duration / 1000).toFixed(1) + 's');
+
+			if (distance >= 0) {
+				distance = 0;
+			}
+
+			if (distance <= this.navWidth) {
+				distance = this.navWidth;
+			}
+
+			if (this.isSafari() || this.isChrome()) {
+			   this.nav.css('-webkit-transform', 'translate(' + distance + 'px,0)');
+			} else{
+			   this.nav.css('transform', 'translate(' + distance + 'px,0)');
+			}
+
+			if (distance == '0') {
+				this.navCta.addClass('ssm-nav-visible');
+				$('html').addClass('is-navOpen');
+				$('.ssm-overlay').fadeIn();
+			}
+		},
+
+		/**
+		 * Close by click on burger icon
+		 */
+		hideNavigation: function() {
+			this.nav.removeClass('ssm-nav-visible');
+			this.scrollNav(this.navWidth, this.settings.speed);
+			$('html').removeClass('is-navOpen');
+			$('.ssm-overlay').fadeOut();
+		},
+
+		/**
+		 * Open by click on burger icon
+		 */
+		showNavigation: function() {
+			this.nav.addClass('ssm-nav-visible');
+			this.scrollNav(0, this.settings.speed);
+		}
+
+	});
+
+	$.fn[pluginName] = function(options) {
+		return this.each(function() {
+			if (! $.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+			}
+		});
+	};
+
+})(jQuery, window, document);
